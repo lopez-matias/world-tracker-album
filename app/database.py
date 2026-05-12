4ce0d3c8c4652b757c3908bc774afa4ae0cda085
Matias Lopez <mmlopeznunez@gmail.com>
2026-05-12 15:35:19 -0300

    Optimization in database

--- app/database.py
+++ app/database.py
@@ -1,11 +1,14 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy.pool import NullPool, StaticPool
 
 from .config import settings
 
+_is_sqlite = "sqlite" in settings.DATABASE_URL
 engine = create_engine(
     settings.DATABASE_URL,
-    connect_args={"check_same_thread": False} if "sqlite" in settings.DATABASE_URL else {},
+    connect_args={"check_same_thread": False} if _is_sqlite else {},
+    poolclass=StaticPool if _is_sqlite else NullPool,
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
